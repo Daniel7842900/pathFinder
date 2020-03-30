9e407a8ea536b42a94a1959c30b52d85018c1fbb
Daniel Lim <daniellim@Daniels-MacBook-Pro.local>
2020-03-29 22:45:34 -0700

    first commit

---
+++ src/Component/PathFinder.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Node from "./Node/Node";
+
+import "./PathFinder.css";
+
+class PathFinder extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <Node></Node>
+      </div>
+    );
+  }
+}
+
+export default PathFinder;
